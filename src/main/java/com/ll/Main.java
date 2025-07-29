8dd21527acc5eb4627fbb91fbba3ac5d6b5c5928
sik2 <bickwang123@gmail.com>
2025-07-29 15:46:20 +0900

    step 1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,23 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("== 명언 앱 ==");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
