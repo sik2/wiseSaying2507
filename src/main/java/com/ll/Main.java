b6b228d6c61b7b3cbf4508a0f392616154e77c7a
sik2 <bickwang123@gmail.com>
2025-07-29 15:53:09 +0900

    step 2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -15,6 +15,12 @@ public class Main {
             if (cmd.equals("종료")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            } else  if (cmd.equals("등록")) {
+                System.out.print("명언: ");
+                String wiseSayingContent = scanner.nextLine().trim();
+
+                System.out.print("작가: ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
             }
         }
 
