744f25f4e4c25e62e159c7f7f5f6bbf903523df1
sik2 <bickwang123@gmail.com>
2025-07-30 10:24:22 +0900

    step 4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
@@ -22,7 +23,10 @@ public class Main {
                 System.out.print("작가: ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                int no = ++lastId;
+
+//               System.out.printf("%d번 명언이 등록되었습니다.\n", no);
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(no) );
             }
         }
 
