50141e1ec3bb67e26d7d8974dcbd1129fb576bcd
sik2 <bickwang123@gmail.com>
2025-07-31 12:41:04 +0900

    step2

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,32 +1,25 @@
 package com.ll;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Rq {
     private final String actionName;
     private final Map<String, String> paramsMap;
 
     public Rq(String cmd) {
-        paramsMap = new HashMap<>();
-
         String[] cmdBits = cmd.split("\\?", 2);
         actionName = cmdBits[0];
         String queryString = cmdBits.length > 1 ? cmdBits[1].trim() : "";
 
-        String[] queryStringBits = queryString.split("&");
-
-        for (String queryParam : queryStringBits) {
-            String[] queryParamBits = queryParam.split("=", 2);
-            String key = queryParamBits[0].trim();
-            String value = queryParamBits.length > 1 ? queryParamBits[1].trim() : "";
-
-            if (value.isEmpty()) {
-                continue;
-            }
-
-            paramsMap.put(key, value);
-        }
+        paramsMap = Arrays.stream(queryString.split("&"))
+                .map(part -> part.split("=", 2))
+                .filter(bits -> bits.length > 0 && !bits[0].trim().isEmpty()  &&  !bits[1].trim().isEmpty())
+                .collect(Collectors.toMap(
+                        bits -> bits[0].trim(),
+                        bits -> bits[1].trim()
+                ));
     }
 
     public String getActionName() {
