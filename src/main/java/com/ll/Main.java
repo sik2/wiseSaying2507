a1cb21cb738782e950919175c7ffebee22670744
sik2 <bickwang123@gmail.com>
2025-07-31 11:20:34 +0900

    Rq 1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,7 +2,12 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
-        app.run();
+//        App app = new App();
+//        app.run();
+
+        Rq rq = new Rq("목록?searchKeywordType=content&searchKeyword=자바");
+        System.out.println("actionName : " + rq.getActionName());
+        System.out.println("params searchKeywordType : " + rq.getParam("searchKeywordType", ""));
+        System.out.println("params searchKeyword : " + rq.getParam("searchKeyword", ""));
     }
 }
---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,43 @@
+package com.ll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+    private final String actionName;
+    private final Map<String, String> paramsMap;
+
+    public Rq(String cmd) {
+        paramsMap = new HashMap<>();
+
+        String[] cmdBits = cmd.split("\\?", 2);
+        actionName = cmdBits[0];
+        String queryString = cmdBits.length > 1 ? cmdBits[1].trim() : "";
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (String queryParam : queryStringBits) {
+            String[] queryParamBits = queryParam.split("=", 2);
+            String key = queryParamBits[0].trim();
+            String value = queryParamBits.length > 1 ? queryParamBits[1].trim() : "";
+
+            if (value.isEmpty()) {
+                continue;
+            }
+
+            paramsMap.put(key, value);
+        }
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public String getParam(String paramName, String defaultValue) {
+        if (paramsMap.containsKey(paramName)) {
+            return paramsMap.get(paramName);
+        } else {
+            return defaultValue;
+        }
+    }
+}
