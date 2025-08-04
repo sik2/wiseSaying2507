08ac3b52014eb3e4c07aa8f864d96b556d691460
sik2 <bickwang123@gmail.com>
2025-08-04 12:08:22 +0900

    static block

--- src/main/java/com/ll/AppContext.java
+++ src/main/java/com/ll/AppContext.java
@@ -9,12 +9,21 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class AppContext {
-    public static final Scanner scanner = new Scanner(System.in);
-    public static final DateTimeFormatter forPrintDateTimeFormatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
-    public static final WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
-    public static final WiseSayingService wiseSayingService = new WiseSayingService();
-    public static final WiseSayingController wiseSayingController = new WiseSayingController();
-    public static final SystemController systemController = new SystemController();
+    public static final Scanner scanner;
+    public static final DateTimeFormatter forPrintDateTimeFormatter;
+    public static final WiseSayingRepository wiseSayingRepository;
+    public static final WiseSayingService wiseSayingService;
+    public static final WiseSayingController wiseSayingController;
+    public static final SystemController systemController;
+
+    static {
+        scanner = new Scanner(System.in);
+        forPrintDateTimeFormatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        wiseSayingRepository = new WiseSayingRepository();
+        wiseSayingService = new WiseSayingService();
+        wiseSayingController = new WiseSayingController();
+        systemController = new SystemController();
+    }
 }
 
 
